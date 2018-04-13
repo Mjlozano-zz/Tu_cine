be2a196843e25a48ca51b0dbb7fbf7349c82875b
mjlozano <hackbin.13>
2018-04-13 11:43:23 -0500

    Homepage added

--- src/tu_cine/FXMLHome_pageController.java
+++ src/tu_cine/FXMLHome_pageController.java
@@ -4,21 +4,22 @@
  * and open the template in the editor.
  */
 package tu_cine;
-
+import javafx.fxml.FXML;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
-
+import com.jfoenix.controls.*;
+import javafx.scene.layout.Background;
 /**
  * FXML Controller class
  *
  * @author JESÚS LOZANO
  */
 public class FXMLHome_pageController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    JFXButton prueba ;
+ 
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
